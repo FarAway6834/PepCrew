cead25317a785243fe6212808dedc9e62dc6c0b9
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-01-09 14:10:30 +0900

    runtime.pm 업데이트

--- pcrew/runtime.pm
+++ pcrew/runtime.pm
@@ -18,6 +18,14 @@ pepcrew-runtime function generate closer
 pepcrew(pr/[pettern]/, "[repl]")(input)
 ```
 
+pepcrew edit input var as regex, then return it.
+
+actually, input var is changed, but I think that's not matter.
+
+because it isn't weird. perl-language is formal language. "weird" is undefined.
+
+it's normal semmentics on perl.
+
 =end comment
 =cut
 sub pepcrew {
