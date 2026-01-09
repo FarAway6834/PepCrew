792b0071467acf7e24d82dd8ebb945ffca9e4f4a
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-01-09 12:37:41 +0900

    runtime.pm 만들기

---
+++ pcrew/runtime.pm
@@ -0,0 +1,15 @@
+package pcrew::Runtime;
+
+use strict;
+use warnings;
+use Exporter 'import';
+
+use pcrew::MyIO;
+
+our @EXPORT = qw(io pepcrew);
+
+sub pepcrew {
+    my ($pettern, $repl) = @_;
+}
+
+1;
