165757fc2ff962555b0a74a6e1adf777cc10376a
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-01-09 13:49:26 +0900

    myio.pm 업데이트

--- pcrew/myio.pm
+++ pcrew/myio.pm
@@ -6,6 +6,30 @@ use Exporter 'import';
 
 our @EXPORT = qw(io);
 
+=begin comment
+
+# io
+
+io is I/O function
+
+## I : input
+
+`io()` is actually `input()` on python
+
+I think that it's better than python
+
+fun fact : I'm actually python programmer
+
+## O : output
+
+`io(some_string)` is actually `print(some_string)` on python
+
+only one argument is allowed
+
+I think that this I/O interface is better simple and pretty good than python or perl
+
+=end commment
+=cut
 sub io {
     if (scalar @_) {
         print $_[0];
